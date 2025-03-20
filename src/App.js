7e102cd7a1575b73e042ea23a6e5ed872ab0255e
jen <904Jen@gmail.com>
2025-03-19 20:21:25 -0400

    Add logo to site

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/cklogo.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <h1>Kitty Automation Services</h1>
         <p>We provide top-notch automation services for your feline friends.</p>
         <p>From automated litter boxes to self-filling food dispensers, we've got you covered.</p>
-        <img src="https://placekitten.com/200/300" alt="A cute kitten" />
       </header>
     </div>
   );
